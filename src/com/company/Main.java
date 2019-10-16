0e858030318e580e1df49c6b42242ddc797c9df2
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-16 13:15:43 +1300

    double challenge

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double pound = 100;
+        double kilo = 0.45359237;
+        System.out.println("100 pounds is " + (kilo * pound) + " kilo");
+
+    }
+}
